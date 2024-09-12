5af6926f4616eab07c9ffa3be3669ff22a6e2113
Juampi270302 <juampylambertucci2002@gmail.com>
2024-09-12 13:16:50 -0300

    Firebase implementado con credenciales de proyecto

--- pruebaReactFirebase/src/Components/FormChargeImages.jsx
+++ pruebaReactFirebase/src/Components/FormChargeImages.jsx
@@ -1,42 +1,121 @@
 import appFirebase from "../credenciales.js";
 
 import {getStorage, ref, uploadBytes, getDownloadURL} from "firebase/storage";
+import {useEffect, useState} from "react";
+import {set} from "react-hook-form";
 
 const storage = getStorage(appFirebase);
 
 const FormChargeImages = () => {
 
-    let urlImgDownload;
 
-    const saveInfo = async (e) => {
-        console.log(urlImgDownload)
-        // funcion de guardado
+
+    const [files, setFiles] = useState([])
+    const [peso, setPeso] = useState(0)
+    const [filesUrls, setFilesUrls] = useState([])
+
+    // useEffect(() => {
+    //     files.forEach((file) => {
+    //         setPeso(peso + (file.size / 1024 / 1024))
+    //     })
+    // }, [files]);
+
+    // useEffect(() => {
+    //     if (peso > 10) {
+    //         alert("El peso de las imagenes no puede superar los 10MB")
+    //     }
+    // }, [peso]);
+
+    const saveInfo = async () => {
+        for (const file of files) {
+
+             if (peso > 10) {
+                 alert(`El peso de las imagenes es mayor a 10 MB. Peso actual: ${peso}`)
+                 return
+             }
+
+            //cargar imagen al storage
+
+            const refImagen = ref(storage, `images/${file.name}`)
+            await uploadBytes(refImagen, file)
+
+            //obtener url de la imagen y guardarlas en el array de imagenes
+            let urlImgDownload = await getDownloadURL(refImagen)
+            setFilesUrls([...filesUrls, urlImgDownload])
+            setFiles([])
+            setPeso(0)
+            console.log(urlImgDownload)
+        }
     }
 
     const fileHandler = async (e) => {
-        //extraer imagen
+
+        //extraer imagen e indice donde se coloca la nueva imagen (por si se modifica alguna)
+        const indice = e.target.id
         const imagen = e.target.files[0]
+        const types = ["image/jpeg", "image/png", "image/jpg"]
 
-        //cargar imagen al storage
-        const refImagen = ref(storage, `images/${imagen.name}`)
-        await uploadBytes(refImagen, imagen)
+        if (imagen === undefined) {
+            return
+        }
 
-        //obtener url de la imagen
-        urlImgDownload = await getDownloadURL(refImagen)
-        console.log(urlImgDownload)
+        if (!types.includes(imagen.type)) {
+            alert("El archivo no es una imagen")
+            return
+        }
+
+        console.log(imagen.type)
+
+        //Aca valido si esta tratando de modificar una imagen
+        if (files[indice - 1] !== undefined) {
+            const imagenActual = files[indice - 1]
+            // Aca valido si el nuevo peso superaria los 10mb, si lo supera, no se guarda la imagen
+            if ((peso - (imagenActual.size / 1024 / 1024) + (imagen.size / 1024 / 1024)) > 10){
+                alert("El peso de las imagenes con la nueva imagen no puede superar los 10MB")
+                return
+            } else {
+                // Aca si el peso con la nueva imagen es valido, actualizo el peso
+                setPeso(peso - (imagenActual.size / 1024 / 1024) + (imagen.size / 1024 / 1024))
+            }
+        // Aca valido si el peso con una nueva imagen es valido
+        } else if ( peso + (imagen.size / 1024 / 1024) > 10) {
+            alert("El peso de las imagenes no puede superar los 10MB")
+            return
+        // En caso de que no se este modificando una imagen y el peso sea valido, actualizo el peso
+        } else {
+            setPeso(peso + (imagen.size / 1024 / 1024))
+        }
+
+        //guardar imagen en el estado
+        const newFiles = [...files]
+        newFiles[indice - 1] = imagen
+        setFiles(newFiles)
     }
 
     return (
-        <div>
-            <form >
-                <div className="input-group">
-                    <input type="file" className="form-control" id="file"
-                           aria-describedby="inputGroupFileAddon04" aria-label="Upload"
-                           onChange={fileHandler}/>
-                    <button className="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04" onClick={saveInfo}>Subir imagen
-                    </button>
-                </div>
-            </form>
+        <div className="row d-flex justify-content-center">
+                <form>
+                    <div className="input-group">
+                        <div className="row">
+                            <input type="file" className="form-control" id="1"
+                                   aria-describedby="inputGroupFileAddon04" aria-label="Upload"
+                                   onChange={fileHandler}/>
+                        </div>
+                        <div className="row">
+                            <input type="file" className="form-control" id="2"
+                                   aria-describedby="inputGroupFileAddon04" aria-label="Upload" placeholder="A"
+                                   onChange={fileHandler}/>
+                        </div>
+                        <div className="row">
+                            <input type="file" className="form-control" id="3"
+                                   aria-describedby="inputGroupFileAddon04" aria-label="Upload"
+                                   onChange={fileHandler} />
+                        </div>
+                        <button className="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04"
+                        onClick={saveInfo}>Subir imagen
+                        </button>
+                    </div>
+                </form>
         </div>
     );
 };
--- pruebaReactFirebase/src/credenciales.js
+++ pruebaReactFirebase/src/credenciales.js
@@ -4,15 +4,13 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA7vJVy62Hb0yuqZoIPzqdXZDRmU8G-W_s",
-    authDomain: "tango-app-grupo10.firebaseapp.com",
-    projectId: "tango-app-grupo10",
-    storageBucket: "tango-app-grupo10.appspot.com",
-    messagingSenderId: "228852715455",
-    appId: "1:228852715455:web:5604afb7b5a6ea134a5b0a",
-    measurementId: "G-7S12J13Y9C"
+    apiKey: "AIzaSyBwuGSqUrqsLNvnI2xMeTmNyfmJuDA5mVM",
+    authDomain: "tango-app-16021.firebaseapp.com",
+    projectId: "tango-app-16021",
+    storageBucket: "tango-app-16021.appspot.com",
+    messagingSenderId: "158499313384",
+    appId: "1:158499313384:web:7d2626aa0c9e83d76d9b5d"
 };
 
 // Initialize Firebase
